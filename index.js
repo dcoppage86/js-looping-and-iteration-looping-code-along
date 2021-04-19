ef31944160b70ffd54d69334fb916f220cf71e10
dcoppage86 <dakotakcoppage@me.com>
2021-04-18 21:58:17 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const namesArray = ["Lisa", "Kaitlyn", "Jan"]
+
+const celebrate = "surprise"
+
+function writeCards(namesArray, celebrate) {
+    let cards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${celebrate} gift!`)
+    }
+    return cards;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0){
+        console.log(countDown);
+        countDown -= 1;
+    }
+    console.log
+}
